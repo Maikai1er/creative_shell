d0b2b29a8f072da17fadd062c1720043ebe061db
Maikai1er <kolesnichenko.misha12@gmail.com>
2024-04-22 21:04:06 +0200

    made the server run XD

--- creative_shell/settings.py
+++ creative_shell/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'creative_shell.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cultural_heritage',
+        'NAME': 'postgres',
         'USER': 'admin',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
