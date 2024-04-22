505dccdd421208a1af614d15362521bb5305d6b8
Maikai1er <kolesnichenko.misha12@gmail.com>
2024-04-22 17:36:19 +0200

    added model for cultural heritag

--- creative_shell/settings.py
+++ creative_shell/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "cultural_heritage.apps.CulturalHeritageConfig"
 ]
 
 MIDDLEWARE = [
---
+++ cultural_heritage/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class CulturalHeritage(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    description = models.TextField()
+    source_link = models.URLField()
+    # image = models.ImageField(upload_to='cultural_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
