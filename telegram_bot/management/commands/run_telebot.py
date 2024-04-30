c8386d2ac306cb3ab5eaa8dc3273dc65829ba1fc
Maikai1er <kolesnichenko.misha12@gmail.com>
2024-04-30 11:16:12 +0200

    renamed telegram bot directory so that it doesn't conflict with the telebot package

---
+++ telegram_bot/management/commands/run_telebot.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand
+from telebot import TeleBot
+
+
+class Command(BaseCommand):
+    help = 'Runs the Telegram bot'
+
+    def handle(self, *args, **options):
+        TOKEN = '6962411316:AAHb_QYx3XU-JNib6gkhDhOXKEBiW_k6s74'
+        bot = TeleBot(TOKEN)
+
+        @bot.message_handler(commands=['start'])
+        def send_welcome(message):
+            bot.reply_to(message, 'Welcome!')
+
+        @bot.message_handler(func=lambda message: True)
+        def echo_message(message):
+            bot.reply_to(message, message.text)
+
+        bot.infinity_polling()
+
+# что по большому счету нужно
+# 1. бот здоровается
+# 2. бот запускается на сервере и сидит запущенным
+# 3. один раз в какое-то время (допустим, день), запускается программа по поиску новых ОКН
+# 4. после этого нам нужна валидация полученной информации
+# 5. после валидации полученная информация разбивается на куски (объекты) и по одному передается
+# в функцию в боте, которая превратит сырую информацию в удобоваримую для пользователя
+# 6. После этого, бот посылает в чат сообщение о том, что был найден новый объект (1 сообщение для 1 объекта)
+# 7. И нам нужно в целом 2 кнопки (пока 2). Первая - апрувнуть объект. Вторая - отклонить объект
+# 8. Если объект апрувнут - информация передается дальше в функцию, которая запишет его в БД
+# 9. Если объект отклонен - информация уничтожается
