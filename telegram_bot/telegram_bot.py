e6db15864b4f27544f28d0213137c6b13eea59ad
Maikai1er <kolesnichenko.misha12@gmail.com>
2024-05-07 14:46:57 +0200

    added default bot reply, made code a bit cleaner

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -1,7 +1,6 @@
-import json
-
 from telebot import TeleBot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+import json
 import redis
 
 TOKEN = '6962411316:AAHb_QYx3XU-JNib6gkhDhOXKEBiW_k6s74'
@@ -17,12 +16,10 @@ redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db, 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, 'Welcome!')
-
-
-@bot.message_handler(commands=['run'])
-def start_bot():
-    bot.polling()
+    first_name = message.from_user.first_name
+    last_name = message.from_user.last_name
+    full_name = f'{first_name} {last_name}' if last_name else first_name
+    bot.reply_to(message, f'Welcome, {full_name}!')
 
 
 @bot.message_handler(commands=['stop'])
@@ -38,26 +35,25 @@ def receive_new_object(heritage):
     bot.send_message(chat_id='5787733609', text='Choose an action:', reply_markup=create_keyboard(['approve', 'reject']))
 
 
-@bot.message_handler(func=lambda message: message.text in ['approve', 'reject'])
+@bot.message_handler(func=lambda message: message.text in ['approve', 'reject', '/approve', '/reject'])
 def handle_decision(message):
     try:
         heritage_json = redis_client.get('current_heritage')
-        print(heritage_json)
         heritage = json.loads(heritage_json)
         if not heritage_json:
             bot.send_message(message.chat.id, 'Error: No current heritage')
             return
 
-        if message.text == 'approve':
+        if message.text == '/approve' or message.text == 'approve':
             from cultural_heritage.save_object_to_database import save_object_to_database
             save_object_to_database(heritage)
             bot.send_message(message.chat.id, 'Heritage successfully saved to database!')
-        elif message.text == 'reject':
+        if message.text == '/reject' or message.text == 'reject':
             bot.send_message(message.chat.id, 'Heritage is rejected and will not be saved to database!')
 
         redis_client.delete('current_heritage')
-    except Exception as e:
-        bot.send_message(message.chat.id, 'Error: ' + str(e))
+    except TypeError:
+        bot.send_message(message.chat.id, 'TypeError, most likely, there is no current heritage to moderate.')
 
 
 def create_keyboard(buttons):
@@ -67,23 +63,11 @@ def create_keyboard(buttons):
     return keyboard
 
 
-def send_notification(text):
-    bot.send_message(chat_id='5787733609', text=text)
+@bot.message_handler(func=lambda message: True)
+def default_reply(message):
+    bot.reply_to(message, text='I don\'t understand, please go talk to ChatGPT <3.')
 
 
 def run_telebot():
     print('Starting telebot ...')
     bot.polling()
-
-
-# что по большому счету нужно
-# 1. бот здоровается
-# 2. бот запускается на сервере и сидит запущенным
-# 3. один раз в какое-то время (допустим, день), запускается программа по поиску новых ОКН
-# 4. после этого нам нужна валидация полученной информации
-# 5. после валидации полученная информация разбивается на куски (объекты) и по одному передается
-# в функцию в боте, которая превратит сырую информацию в удобоваримую для пользователя
-# 6. После этого, бот посылает в чат сообщение о том, что был найден новый объект (1 сообщение для 1 объекта)
-# 7. И нам нужно в целом 2 кнопки (пока 2). Первая - апрувнуть объект. Вторая - отклонить объект
-# 8. Если объект апрувнут - информация передается дальше в функцию, которая запишет его в БД
-# 9. Если объект отклонен - информация уничтожается
