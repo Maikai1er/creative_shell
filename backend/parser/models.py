cdcb5f456ad31e61e2be9b48c41d75acb15524db
Maikai1er <kolesnichenko.misha12@gmail.com>
2024-07-22 17:22:05 +0200

    refactored parser model to match data management fields

--- backend/parser/models.py
+++ backend/parser/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class ParsedData(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    year_whs = models.IntegerField(default=2024, null=True)
     year_endangered = models.IntegerField(default=2024, null=True)
+    reason = models.TextField(null=True, blank=True)
+    image_path = models.CharField(max_length=255, null=True, blank=True)
 
     class Meta:
         db_table = 'parsed_data'
