3d71879e42a6808dbc090d2964eb4d8a8e8066de
Maikai1er <kolesnichenko.misha12@gmail.com>
2024-07-23 17:46:08 +0200

    repaired admin static... again

--- backend/creative_shell/settings.py
+++ backend/creative_shell/settings.py
@@ -84,13 +84,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    BASE_DIR / 'app/static'
 ]
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/usr/share/nginx/html/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
